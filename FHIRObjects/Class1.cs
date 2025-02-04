04b7e941fe1b76cbbb2181f9452b23c101a17888
AdamInBoston <ahartwell@harriscomputer.com>
2025-02-04 16:30:25 -0500

    Add project files.

---
+++ FHIRObjects/Class1.cs
@@ -0,0 +1,261 @@
+﻿using Hl7.Fhir.Model;
+
+namespace FHIRObjects
+{
+    public class Class1
+    {
+        Patient GetPatient()
+        {
+            var patient = new Patient();
+
+            return patient;
+        }
+
+        CareTeam GetCareTeam()
+        {
+            var careTeam = new CareTeam();
+
+            return careTeam;
+        }
+
+        Organization GetOrganization()
+        {
+            var organization = new Organization();
+
+            return organization;
+        }
+
+        Practitioner GetPractitioner()
+        {
+            var practitioner = new Practitioner();
+
+            return practitioner;
+        }
+
+        Location GetLocation()
+        {
+            var location = new Location();
+
+            return location;
+        }
+
+        Encounter GetEncounter()
+        {
+            var encounter = new Encounter();
+
+            return encounter;
+        }
+
+        Condition GetCondition()
+        {
+            var condition = new Condition();
+
+            return condition;
+        }
+
+        Observation GetObservation()
+        {
+            var observation = new Observation();
+
+            return observation;
+        }
+
+        Medication GetMedication()
+        {
+            var medication = new Medication();
+
+            return medication;
+        }
+
+        MedicationRequest GetMedicationRequest()
+        {
+            var medicationRequest = new MedicationRequest();
+
+            return medicationRequest;
+        }
+
+        MedicationAdministration GetMedicationAdministration()
+        {
+            var medicationAdministration = new MedicationAdministration();
+
+            return medicationAdministration;
+        }
+
+        MedicationDispense GetMedicationDispense()
+        {
+            var medicationDispense = new MedicationDispense();
+
+            return medicationDispense;
+        }
+
+        MedicationStatement GetMedicationStatement()
+        {
+            var medicationStatement = new MedicationStatement();
+
+            return medicationStatement;
+        }
+
+        Procedure GetProcedure()
+        {
+            var procedure = new Procedure();
+
+            return procedure;
+        }
+
+        DiagnosticReport GetDiagnosticReport()
+        {
+            var diagnosticReport = new DiagnosticReport();
+
+            return diagnosticReport;
+        }
+
+        DocumentReference GetDocumentReference()
+        {
+            var documentReference = new DocumentReference();
+
+            return documentReference;
+        }
+
+        Immunization GetImmunization()
+        {
+            var immunization = new Immunization();
+
+            return immunization;
+        }
+
+        AllergyIntolerance GetAllergyIntolerance()
+        {
+            var allergyIntolerance = new AllergyIntolerance();
+
+            return allergyIntolerance;
+        }
+
+        Goal GetGoal()
+        {
+            var goal = new Goal();
+
+            return goal;
+        }
+
+        CarePlan GetCarePlan()
+        {
+            var carePlan = new CarePlan();
+
+            return carePlan;
+        }
+
+        Questionnaire GetQuestionnaire()
+        {
+            var questionnaire = new Questionnaire();
+
+            return questionnaire;
+        }
+
+        QuestionnaireResponse GetQuestionnaireResponse()
+        {
+            var questionnaireResponse = new QuestionnaireResponse();
+
+            return questionnaireResponse;
+        }
+
+        Device GetDevice()
+        {
+            var device = new Device();
+
+            return device;
+        }
+
+        DeviceRequest GetDeviceRequest()
+        {
+            var deviceRequest = new DeviceRequest();
+
+            return deviceRequest;
+        }
+
+        DeviceUseStatement GetDeviceUseStatement()
+        {
+            var deviceUseStatement = new DeviceUseStatement();
+
+            return deviceUseStatement;
+        }
+
+        Communication GetCommunication()
+        {
+            var communication = new Communication();
+
+            return communication;
+        }
+
+        CommunicationRequest GetCommunicationRequest()
+        {
+            var communicationRequest = new CommunicationRequest();
+
+            return communicationRequest;
+        }
+
+        Appointment GetAppointment()
+        {
+            var appointment = new Appointment();
+
+            return appointment;
+        }
+
+        AppointmentResponse GetAppointmentResponse()
+        {
+            var appointmentResponse = new AppointmentResponse();
+
+            return appointmentResponse;
+        }
+
+        Schedule GetSchedule()
+        {
+            var schedule = new Schedule();
+
+            return schedule;
+        }
+
+        Slot GetSlot()
+        {
+            var slot = new Slot();
+
+            return slot;
+        }
+
+        HealthcareService GetHealthcareService()
+        {
+            var healthcareService = new HealthcareService();
+
+            return healthcareService;
+        }
+
+        Claim GetClaim()
+        {
+            var claim = new Claim();
+
+            return claim;
+        }
+
+        ClaimResponse GetClaimResponse()
+        {
+            var claimResponse = new ClaimResponse();
+
+            return claimResponse;
+        }
+
+        ExplanationOfBenefit GetExplanationOfBenefit()
+        {
+            var explanationOfBenefit = new ExplanationOfBenefit();
+
+            return explanationOfBenefit;
+        }
+
+        Coverage GetCoverage()
+        {
+            var coverage = new Coverage();
+
+            return coverage;
+        }
+
+
+    }
+}
---
+++ FHIRObjects/SQLRepos/PatientRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using FHIRObjects.SQLRepoDefinitions;
+using Hl7.Fhir.Model;
+using Microsoft.Data.SqlClient;
+
+namespace FHIRObjects.SQLRepos
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly string _connectionString;
+
+        public PatientRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Patient GetPatientByName(string name)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection())
+                {
+                    string query = "SELECT * FROM Addendum WHERE id = @id";
+
+                    var patient = cn.QueryFirstOrDefault<Patient>(query, new { id = name }) ?? new Patient();
+
+                    return patient;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Patient GetPatientById(string id)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection())
+                {
+                    string query = "SELECT * FROM Demographics WHERE PatientID = @id";
+
+                    var patient = cn.QueryFirstOrDefault<Patient>(query, new { id = id }) ?? new Patient();
+
+                    return patient;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
